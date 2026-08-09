4e6dcff941611847862267b4e356944bbfa3e9fd
Chris Valle <christophervalle3338@gmail.com>
2026-08-08 19:33:20 -0700

    fix: update API_BASE_URL to point to local development server

--- js/config.js
+++ js/config.js
@@ -1,1 +1,2 @@
-const API_BASE_URL = 'https://salon-website-backend-z0m2.onrender.com';
+const API_BASE_URL = 'http://chris-fedora:8080';
+// const API_BASE_URL = 'https://salon-website-backend-z0m2.onrender.com/';
