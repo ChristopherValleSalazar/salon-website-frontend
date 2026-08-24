c25d1e992a62d02f6cc8705e1d5da8bb787ecf92
Chris Valle <christophervalle3338@gmail.com>
2026-08-23 23:33:51 -0700

    fix: update API_BASE_URL to use localhost for local development

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-// const API_BASE_URL = 'http://chris-fedora:8080';
+const API_BASE_URL = 'http://localhost:8080';
 // const API_BASE_URL = 'https://z5jngjgg-8080.usw3.devtunnels.ms';
-const API_BASE_URL = 'https://api.yasminbeauty.salon';
+// const API_BASE_URL = 'https://api.yasminbeauty.salon';
